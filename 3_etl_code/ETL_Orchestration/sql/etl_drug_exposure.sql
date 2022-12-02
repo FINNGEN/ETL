29a059991e44a30aa1a121623cec37628322a612
shanmukhasampath <shanmukhasampath@gmail.com>
2022-12-02 16:27:52 +0200

    First Round of Drug Exposure Complete

--- 3_etl_code/ETL_Orchestration/sql/etl_drug_exposure.sql
+++ 3_etl_code/ETL_Orchestration/sql/etl_drug_exposure.sql
@@ -35,19 +35,32 @@ INSERT INTO `etl_sam_unittest_omop.drug_exposure`
 	dose_unit_source_value
 )
 
-SELECT ROW_NUMBER() OVER(PARTITION BY p.person_id ORDER by p.person_id,purch.APPROX_EVENT_DAY) AS drug_exposure_id,
+SELECT ROW_NUMBER() OVER(PARTITION BY p.person_id ORDER by p.person_id,vo.visit_occurrence_id,purch.APPROX_EVENT_DAY) AS drug_exposure_id,
 			 p.person_id AS person_id,
-			 relmap.ingredientID AS drug_concept_id,
+			 CASE
+			 		  WHEN fgc.omop_concept_id IS NOT NULL AND relmap.concept_class_id IN ('Branded Pack','Clinical Pack','Branded Drug','Clinical Drug','Branded Drug Component','Clinical Drug Component','Branded Drug Form','Clinical Drug Form')  THEN relmap.concept_id_2
+						 WHEN fgc.omop_concept_id IS NOT NULL AND relmap.concept_class_id NOT IN ('Branded Pack','Clinical Pack','Branded Drug','Clinical Drug','Branded Drug Component','Clinical Drug Component','Branded Drug Form','Clinical Drug Form')  THEN relmap.ingredientID
+						ELSE 0
+			 END AS drug_concept_id,
 			 purch.APPROX_EVENT_DAY AS drug_exposure_start_date,
-			 FORMAT_TIMESTAMP("%F %T",timestamp(purch.APPROX_EVENT_DAY)) AS drug_exposure_start_datetime,
-			 DATE_ADD(purch.APPROX_EVENT_DAY, INTERVAL 1 DAY) AS drug_exposure_end_date,
-			 CAST(NULL AS DATETIME) AS drug_exposure_end_datetime,
+			 DATETIME(TIMESTAMP(purch.APPROX_EVENT_DAY)) AS drug_exposure_start_datetime,
+			 CASE
+			 		  WHEN purch.CODE4_PLKM IS NOT NULL THEN DATE_ADD(purch.APPROX_EVENT_DAY, INTERVAL CAST(relmap.PackageSize AS INT64) * CAST(purch.CODE4_PLKM AS INT64) DAY)
+						ELSE DATE_ADD(purch.APPROX_EVENT_DAY, INTERVAL 29 DAY)
+			 END AS drug_exposure_end_date,
+			 CASE
+			 		  WHEN purch.CODE4_PLKM IS NOT NULL THEN DATETIME(TIMESTAMP(DATE_ADD(purch.APPROX_EVENT_DAY, INTERVAL CAST(relmap.PackageSize AS INT64) * CAST(purch.CODE4_PLKM AS INT64) DAY) ))
+						ELSE DATETIME(TIMESTAMP(DATE_ADD(purch.APPROX_EVENT_DAY, INTERVAL 29 DAY) ))
+			 END AS drug_exposure_end_datetime,
 			 CAST(NULL AS DATE) AS verbatim_end_date,
 			 38000175 AS drug_type_concept_id,
 			 NULL AS stop_reason,
 			 NULL AS refills,
-			 purch.CODE4_PLKM AS quantity,
-			 NULL AS days_supply,
+			 CASE
+			 		  WHEN purch.CODE4_PLKM IS NULL THEN CAST(relmap.PackageSize AS FLOAT64)
+						ELSE CAST(purch.CODE4_PLKM AS FLOAT64)
+			 END AS quantity,
+			 1 AS days_supply,
 			 NULL AS sig,
 			 NULL AS route_concept_id,
 			 NULL AS lot_number,
@@ -56,7 +69,7 @@ SELECT ROW_NUMBER() OVER(PARTITION BY p.person_id ORDER by p.person_id,purch.APP
 			 NULL AS visit_detail_id,
 			 purch.CODE3_VNRO AS drug_source_value,
 			 fgc.omop_concept_id AS drug_source_concept_id,
-			 NULL AS route_source_value,
+			 relmap.AdministrationRoute AS route_source_value,
 			 NULL AS dose_unit_source_value
 FROM `etl_dev_input.purch` AS purch
 JOIN `etl_sam_unittest_omop.person` AS p
@@ -71,39 +84,63 @@ JOIN
 WITH drugInfo AS (
 select c.concept_code, c.concept_name, r.concept_class_id,
 			 c.concept_id, cr.relationship_id, cr.concept_id_2,
-			 vnr.ATC, vnr.AdministrationRoute, vnr.DosageForm,
+			 vnr.ATC, vnr.AdministrationRouteSourceTextFI ,vnr.AdministrationRoute, vnr.DosageForm,
 			 vnr.PackageSize,vnr.PackageFactor, vnr.PackageUnit,
 			 vnr.Substance, t1.ingredientID, vnr.Strength
 from `etl_sam_unittest_omop.concept` as c
 JOIN `etl_sam_unittest_omop.concept_relationship` as cr
 ON c.concept_id = cr.concept_id_1 AND cr.relationship_id = 'Maps to'
 JOIN `medical_codes.finngen_vnr_v1` as vnr
-ON CAST(vnr.VNR AS STRING) = c.concept_code
+ON LPAD(CAST(vnr.VNR AS STRING),6,'0') = c.concept_code
 JOIN `etl_sam_unittest_omop.concept` as r
-ON r.concept_id = cr.concept_id_2
-JOIN
+ON r.concept_id = cr.concept_id_2 AND r.concept_class_id IN ('Branded Pack','Clinical Pack','Branded Drug','Clinical Drug','Branded Drug Comp','Clinical Drug Comp','Branded Drug Form','Clinical Drug Form','Ingredient')#,'Quant Clinical Drug','Quantified Branded Drug','Clinical Drug Box','Quantified Clinical Box')
+FULL JOIN
 (
 	WITH finalConnection AS(
 		SELECT concept_id_1, relationship_id, concept_id_2
 		FROM etl_sam_unittest_omop.concept_relationship
-		WHERE relationship_id='Consists of'
+		WHERE relationship_id IN ('Consists of','RxNorm has ing')
 	)
 	SELECT fc.concept_id_1 AS drugID,
-				 fi.concept_id_2 AS ingredientID,
+				 CASE
+				 		  WHEN fc.relationship_id = 'Ingredient' THEN fc.concept_id_1
+							ELSE fi.concept_id_2
+					END AS ingredientID,
 				 fcr.concept_name AS ingredientName
 	FROM finalConnection as fc
 	JOIN etl_sam_unittest_omop.concept_relationship AS fi
-	ON fc.concept_id_2=fi.concept_id_1 AND fi.relationship_id='RxNorm has ing'
+	ON fc.concept_id_2=fi.concept_id_1 AND (fi.relationship_id='RxNorm has ing' OR fc.relationship_id='RxNorm has ing')
 	JOIN etl_sam_unittest_omop.concept AS fcr
 	ON fcr.concept_id = fi.concept_id_2
 	ORDER BY fc.concept_id_1
 ) AS t1
 ON t1.drugID = cr.concept_id_2 AND t1.ingredientName = vnr.Substance
 )
-select *
-from drugInfo
+SELECT DISTINCT *
+FROM drugInfo
+WHERE concept_code IS NOT NULL
+ORDER BY concept_code,
+				 (
+					 CASE WHEN concept_class_id = 'Branded Pack' THEN 1
+					  		WHEN concept_class_id = 'Clinical Pack' THEN 2
+					  		WHEN concept_class_id = 'Branded Drug' THEN 3
+					  		WHEN concept_class_id = 'Clinical Drug' THEN 4
+					  		WHEN concept_class_id = 'Branded Drug Comp' THEN 5
+								WHEN concept_class_id = 'Clinical Drug Comp' THEN 6
+								WHEN concept_class_id = 'Branded Drug Form' THEN 7
+								WHEN concept_class_id = 'Clinical Drug Form' THEN 8
+								WHEN concept_class_id = 'Ingredient' THEN 9
+								#WHEN concept_class_id = 'Quant Clinical Drug' THEN 9
+								#WHEN concept_class_id = 'Quantified Branded Drug' THEN 10
+								#WHEN concept_class_id = 'Clinical Drug Box' THEN 11
+								#WHEN concept_class_id = 'Quantified Clinical Box' THEN 12
+								#ELSE 13
+								ELSE 10
+					 END
+					)
 
 
 ) AS relmap
 ON purch.CODE3_VNRO=relmap.concept_code
+ORDER BY person_id
 
