5a33bfd19fb3e730d8afb23227ad9326dc93facd
shanmukhasampath <shanmukhasampath@gmail.com>
2023-02-24 16:33:24 +0200

    Observation SQL and Tests added. No non-standard map test fail but will show PASS when correct test function used.

---
+++ 3_etl_code/ETL_Orchestration/sql/etl_observation.sql
@@ -0,0 +1,114 @@
+# DESCRIPTION:
+# Creates a row in cdm-observation table for each FinnGen id in the registries HILMO and PRIM_OUT.
+# Person id is extracted from person table
+#
+#
+# PARAMETERS:
+#
+# - schema_etl_input: schema with the etl input tables
+# - schema_cdm_output: schema with the output CDM tables
+
+TRUNCATE TABLE @schema_cdm_output.observation;
+INSERT INTO @schema_cdm_output.observation
+(
+    observation_id,
+    person_id,
+    observation_concept_id,
+    observation_date,
+    observation_datetime,
+    observation_type_concept_id,
+    value_as_number,
+    value_as_string,
+    value_as_concept_id,
+    qualifier_concept_id,
+    unit_concept_id,
+    provider_id,
+    visit_occurrence_id,
+    visit_detail_id,
+    observation_source_value,
+    observation_source_concept_id,
+    unit_source_value,
+    qualifier_source_value,
+    value_source_value,
+    observation_event_id,
+    obs_event_field_concept_id
+)
+
+WITH
+# 1 - Get only Observation events from HILMO and PRIM_OUT registries, as define form standard code or using domain
+#   - Add observaton standard concept id.
+observation_from_registers_with_source_and_standard_concept_id AS (
+  SELECT sme.*,
+         cmap.concept_id_2
+  FROM @schema_etl_input.stem_medical_events AS sme
+  LEFT JOIN (
+    SELECT cr.concept_id_1, cr.concept_id_2, c.domain_id
+    FROM @schema_vocab.concept_relationship AS cr
+    JOIN @schema_vocab.concept AS c
+    ON cr.concept_id_2 = c.concept_id
+    WHERE cr.relationship_id = 'Maps to' AND c.domain_id ='Observation'
+  ) AS cmap
+  ON CAST(sme.omop_source_concept_id AS INT64) = cmap.concept_id_1
+  # Here look for default domain observation and standard domain to be observation
+  WHERE sme.SOURCE IN ('INPAT','OPER_IN','OPER_OUT','OUTPAT','PRIM_OUT') AND
+        sme.default_domain LIKE '%Obs%' AND (cmap.domain_id = 'Observation' OR cmap.domain_id IS NULL)
+)
+
+# 2 - Shape into observation table
+SELECT
+# observation_id
+  ROW_NUMBER() OVER(ORDER by ofrwsasci.SOURCE, ofrwsasci.INDEX) AS observation_id,
+# person_id
+  p.person_id AS person_id,
+# observation_concept_id
+  CASE
+      WHEN ofrwsasci.concept_id_2 IS NOT NULL THEN ofrwsasci.concept_id_2
+      ELSE 0
+  END AS observation_concept_id,
+# observation_date
+  ofrwsasci.APPROX_EVENT_DAY AS observation_date,
+# observation_datetime
+  DATETIME(TIMESTAMP(ofrwsasci.APPROX_EVENT_DAY)) AS observation_datetime,
+# observation_type_concept_id
+  32879 AS observation_type_concept_id,
+# value_as_number
+  NULL AS value_as_number,
+# value_as_string
+  CAST(NULL AS STRING) AS value_as_string,
+# value_as_concept_id
+  NULL AS value_as_concept_id,
+# qualifier_concept_id
+  NULL AS qualifier_concept_id,
+# unit_concept_id
+  NULL AS unit_concept_id,
+# provider_id
+  vo.provider_id AS provider_id,
+# visit_occurrence_id
+  vo.visit_occurrence_id AS visit_occurrence_id,
+# visit_detail_id
+  0 AS visit_detail_id,
+# observation_source_value
+  ofrwsasci.CODE1 AS observation_source_value,
+# observation_source_concept_id
+CASE
+  WHEN ofrwsasci.omop_source_concept_id IS NULL THEN 0
+  ELSE CAST(ofrwsasci.omop_source_concept_id AS INT64)
+END AS observation_source_concept_id,
+# unit_source_value
+  CAST(NULL AS STRING) AS unit_source_value,
+# qualifier_source_value
+  CAST(NULL AS STRING) AS qualifier_source_value,
+# value_source_value
+  CAST(NULL AS STRING) AS value_source_value,
+# observation_event_id
+  NULL AS observation_event_id,
+# obs_event_field_concept_id
+  NULL AS obs_event_field_concept_id
+FROM observation_from_registers_with_source_and_standard_concept_id AS ofrwsasci
+JOIN @schema_cdm_output.person AS p
+ON p.person_source_value = ofrwsasci.FINNGENID
+JOIN @schema_cdm_output.visit_occurrence AS vo
+ON vo.person_id = p.person_id AND
+   CONCAT('SOURCE=',ofrwsasci.SOURCE,';INDEX=',ofrwsasci.INDEX) = vo.visit_source_value AND
+   ofrwsasci.APPROX_EVENT_DAY = vo.visit_start_date
+ORDER BY person_id, observation_id;
---
+++ 3_etl_code/ETL_Orchestration/tests/unittest_etl_observation_table.R
@@ -0,0 +1,251 @@
+# DESCRIPTION:
+# Unit tests for etl_observation.
+#
+# PARAMETERS:
+#
+# Test ids: 10xx
+# Finngenids: FG10xxyyy
+
+# Declare Test - 1001 - Default
+declareTest(1001, "etl_observation works with defaults")
+
+add_finngenid_info(
+  finngenid="FG1001001"
+)
+add_hilmo(
+  finngenid = "FG1001001",
+  source = "OUTPAT",
+  event_age = as_subquery(47.42),
+  approx_event_day = "1994-03-08",
+  code1_icd_symptom_operation_code = "W00",
+  category = "EX2",
+  index = "FG1001001-1")
+expect_observation(
+  #observation_id is rand
+  person_id = lookup_person("person_id", person_source_value="FG1001001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1001001"),
+                                                visit_source_value = "SOURCE=OUTPAT;INDEX=FG1001001-1"),
+  observation_concept_id = as_subquery(4053863),
+  observation_date = "1994-03-08",
+  observation_datetime = "1994-03-08T00:00:00",
+  observation_type_concept_id = as_subquery(32879),
+  value_as_number = NULL,
+  value_as_string = NULL,
+  value_as_concept_id = NULL,
+  qualifier_concept_id = NULL,
+  unit_concept_id = NULL,
+  provider_id = as_subquery(0),
+  observation_source_value = "W00",
+  observation_source_concept_id = as_subquery(45756242),
+  unit_source_value = NULL, qualifier_source_value = NULL, value_source_value = NULL,
+  observation_event_id = NULL, obs_event_field_concept_id = NULL
+)
+
+# TESTS MAPPING CODES WITH STANDARD MAPPING  -----------------------------------------------------------------------------
+
+# Declare Test - 1002 - Find standard for an example code in each source in HILMO and PRIM_OUT
+declareTest(1002, "etl_observation adds one event for each code with standard mapping for sources in HILMO and PRIM_OUT registries")
+
+add_finngenid_info(
+  finngenid="FG1002001"
+)
+# INPAT
+add_hilmo(
+  finngenid = "FG1002001",
+  source = "INPAT",
+  code1_icd_symptom_operation_code = "X58",
+  code2_icd_cause_na = NULL,
+  icdver = "10",
+  category = "EX1",
+  index = "FG1002001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1002001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1002001"),
+                                                visit_source_value = "SOURCE=INPAT;INDEX=FG1002001-1"),
+  observation_concept_id  = as_subquery(4305831),
+  observation_source_value = "X58",
+  observation_source_concept_id = as_subquery(45756411)
+)
+
+# OPER_IN
+add_hilmo(
+  finngenid = "FG1002001",
+  source = "OPER_IN",
+  code1_icd_symptom_operation_code = "ZXE10",
+  code2_icd_cause_na = NULL,
+  category = "NOM1",
+  index = "FG1002001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1002001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1002001"),
+                                                visit_source_value = "SOURCE=OPER_IN;INDEX=FG1002001-1"),
+  observation_concept_id = as_subquery(1175267),
+  observation_source_value = "ZXE10",
+  observation_source_concept_id = as_subquery(2011011401)
+)
+
+# OPER_OUT
+add_hilmo(
+  finngenid = "FG1002001",
+  source = "OPER_OUT",
+  code1_icd_symptom_operation_code = "IEY05",
+  code2_icd_cause_na = NULL,
+  category = "NOM1",
+  index = "FG1002001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1002001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1002001"),
+                                                visit_source_value = "SOURCE=OPER_OUT;INDEX=FG1002001-1"),
+  observation_concept_id = as_subquery(4150859),
+  observation_source_value = "IEY05",
+  observation_source_concept_id = as_subquery(2011004224)
+)
+
+# OUTPAT
+add_hilmo(
+  finngenid = "FG1002001",
+  source = "OUTPAT",
+  code1_icd_symptom_operation_code = "C1623",
+  code2_icd_cause_na = NULL,
+  icdver = "10",
+  category = "1",
+  index = "FG1002001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1002001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1002001"),
+                                                visit_source_value = "SOURCE=OUTPAT;INDEX=FG1002001-1"),
+  observation_concept_id = as_subquery(4074210),
+  observation_source_value = "C1623",
+  observation_source_concept_id = as_subquery(2010000525)
+)
+
+# PRIM_OUT
+add_prim_out(
+  finngenid = "FG1002001",
+  source = "PRIM_OUT",
+  code1_code = "B28",
+  icdver = NULL,
+  category = "ICP1",
+  index = "FG1002001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1002001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1002001"),
+                                                visit_source_value = "SOURCE=PRIM_OUT;INDEX=FG1002001-1"),
+  observation_concept_id = as_subquery(4137049),
+  observation_source_value = "B28",
+  observation_source_concept_id = as_subquery(2029000239)
+)
+
+# TESTS MAPPING CODES WITH MULTIPLE STANDARD MAPPING  -----------------------------------------------------------------------------
+
+# Declare Test - 1003 - ICD10 code that maps to two standard codes - one condition and one observation but only outputs observation
+declareTest(1003, "etl_observation adds only one event for a code with standard mapping to 2 codes one condition and one observation")
+
+add_finngenid_info(
+  finngenid="FG1003001"
+)
+add_prim_out(
+  finngenid = "FG1003001",
+  source = "PRIM_OUT",
+  code1_code = "A02",
+  icdver = NULL,
+  category = "ICP1",
+  index = "FG1003001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1003001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1003001"),
+                                                visit_source_value = "SOURCE=PRIM_OUT;INDEX=FG1003001-1"),
+  observation_concept_id = as_subquery(46234710),
+  observation_source_value = "A02",
+  observation_source_concept_id = as_subquery(2029000143)
+)
+
+# Declare Test - 1004 - NCSP code that maps to two standard codes both observation codes
+declareTest(1004, "etl_observation adds two events for a code with standard mapping to two codes both observation")
+
+add_finngenid_info(
+  finngenid="FG1004001"
+)
+add_hilmo(
+  finngenid = "FG1004001",
+  source = "OUTPAT",
+  code1_icd_symptom_operation_code = "V299",
+  index = "FG1004001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1004001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1004001"),
+                                                visit_source_value = "SOURCE=OUTPAT;INDEX=FG1004001-1"),
+  observation_concept_id = as_subquery(437165),
+  observation_source_value = "V299",
+  observation_source_concept_id = as_subquery(45546833)
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1004001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1004001"),
+                                                visit_source_value = "SOURCE=OUTPAT;INDEX=FG1004001-1"),
+  observation_concept_id = as_subquery(4175576),
+  observation_source_value = "V299",
+  observation_source_concept_id = as_subquery(45546833)
+)
+
+# TESTS CODES WITH NON-STANDARD MAPPING BUT WITHOUT STANDARD MAPPING ------------------------------------------------------------
+
+# Declare Test - 1005 - Codes with non-standard mapping and without standard mapping take domain from concept table if not from source and vocab
+declareTest(1005, "etl_observation inserts one event for a code with non-standard mapping in procedure domain and without standard mapping")
+
+add_finngenid_info(
+  finngenid="FG1005001"
+)
+add_hilmo(
+  finngenid = "FG1005001",
+  source = "INPAT",
+  code1_icd_symptom_operation_code = "01797",
+  icdver = "8",
+  category = "1",
+  index = "FG1005001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1005001"),
+  visit_occurrence_id = lookup_visit_occurrence("visit_occurrence_id",
+                                                person_id = lookup_person("person_id",person_source_value = "FG1005001"),
+                                                visit_source_value = "SOURCE=INPAT;INDEX=FG1005001-1"),
+  observation_concept_id = as_subquery(0),
+  observation_source_value = "01797",
+  observation_source_concept_id = as_subquery(2008000250)
+)
+
+# TESTS CODES WITHOUT NON-STANDARD MAPPING --------------------------------------------------------------------------------------
+
+# Declare Test - 1006 - Codes with no mapping are not added to observation currently with observation_concept_id=0. May change in future.
+# This test will fail now but will pass in future.
+declareTest(1006, "etl_observation DOESNOT insert one row in observation even if code has no mapping")
+
+add_finngenid_info(
+  finngenid="FG1006001"
+)
+add_hilmo(
+  finngenid = "FG1006001",
+  source = "OUTPAT",
+  code1_icd_symptom_operation_code = "-1",
+  category = "EX1",
+  index = "FG1006001-1"
+)
+expect_observation(
+  person_id = lookup_person("person_id", person_source_value="FG1006001")
+)
