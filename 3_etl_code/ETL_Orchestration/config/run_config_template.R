51f65d92ead9026d51f0a56d467c5b45a7e3a217
shanmukhasampath <shanmukhasampath@gmail.com>
2023-02-27 15:55:01 +0200

    Added stem table step to run_config file and fixed names.

--- 3_etl_code/ETL_Orchestration/config/run_config_template.R
+++ 3_etl_code/ETL_Orchestration/config/run_config_template.R
@@ -4,10 +4,11 @@ run_config <- tibble::tribble(
   "person_table" , here::here("sql/etl_person_table.sql"), here::here("tests/unittest_etl_person_table.R"), TRUE, FALSE,
   #
   # "observation_period_table" , here::here("sql/etl_observation_period.sql"), here::here("tests/unittest_etl_observation_period_table.R"), TRUE, FALSE,
-  # "visit_occurence_table" , here::here("sql/etl_visit_occurrence.sql"), here::here("tests/unittest_etl_visit_occurrence_table.R"), TRUE, FALSE,
+  # "visit_occurrence_table" , here::here("sql/etl_visit_occurrence.sql"), here::here("tests/unittest_etl_visit_occurrence_table.R"), TRUE, FALSE,
+  # "stem_medical_events" , here::here("sql/etl_stem_medical_events.sql"), "", TRUE, FALSE, # set test always to FALSE
   # "drug_exposure_table" , here::here("sql/etl_drug_exposure.sql"), here::here("tests/unittest_etl_drug_exposure_table.R"), TRUE, FALSE,
-  # "condition_occurence_table" , here::here("sql/etl_condition_occurrence.sql"), here::here("tests/unittest_etl_condition_occurrence_table.R"), TRUE, FALSE,
-  # "procedure_occurence_table" , here::here("sql/etl_procedure_occurrence.sql"), here::here("tests/unittest_etl_procedure_occurrence_table.R"), TRUE, FALSE,
+  # "condition_occurrence_table" , here::here("sql/etl_condition_occurrence.sql"), here::here("tests/unittest_etl_condition_occurrence_table.R"), TRUE, FALSE,
+  # "procedure_occurrence_table" , here::here("sql/etl_procedure_occurrence.sql"), here::here("tests/unittest_etl_procedure_occurrence_table.R"), TRUE, FALSE,
   # "death_table" , here::here("sql/etl_death.sql"), here::here("tests/unittest_etl_death_table.R"), TRUE, FALSE,
   # "measurement_table" , here::here("sql/etl_measurement.sql"), here::here("tests/unittest_etl_measurement_table.R"), TRUE, FALSE,
   # "device_exposure_table" , here::here("sql/etl_device_exposure.sql"), here::here("tests/unittest_etl_device_exposure_table.R"), TRUE, FALSE,
