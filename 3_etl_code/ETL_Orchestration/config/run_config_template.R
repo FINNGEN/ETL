d03206e3c250d0a7bf2e99b92defec3993c248d8
Javier Gracia-Tabuenca <javier.graciatabuenca@tuni.fi>
2023-03-03 11:30:40 +0200

    Revert "Revert "Merge branch 'development' into 34-add-post-processing-achiles-dqd""
    This reverts commit 91c4e64d0e9bc14e1fec1b03ac5257c1e4e57f7e.

--- 3_etl_code/ETL_Orchestration/config/run_config_template.R
+++ 3_etl_code/ETL_Orchestration/config/run_config_template.R
@@ -4,8 +4,8 @@ run_config <- tibble::tribble(
   "person_table" , here::here("sql/etl_person_table.sql"), here::here("tests/unittest_etl_person_table.R"), TRUE, FALSE,
   #
   # "observation_period_table" , here::here("sql/etl_observation_period.sql"), here::here("tests/unittest_etl_observation_period_table.R"), TRUE, FALSE,
-  # "visit_occurrence_table" , here::here("sql/etl_visit_occurrence.sql"), here::here("tests/unittest_etl_visit_occurrence_table.R"), TRUE, FALSE,
   # "provider_table", here::here("sql/etl_provider.sql"), "", TRUE, FALSE,# set test always to FALSE
+  # "visit_occurrence_table" , here::here("sql/etl_visit_occurrence.sql"), here::here("tests/unittest_etl_visit_occurrence_table.R"), TRUE, FALSE,
   # "stem_medical_events" , here::here("sql/etl_stem_medical_events.sql"), "", TRUE, FALSE, # set test always to FALSE
   # "drug_exposure_table" , here::here("sql/etl_drug_exposure.sql"), here::here("tests/unittest_etl_drug_exposure_table.R"), TRUE, FALSE,
   # "condition_occurrence_table" , here::here("sql/etl_condition_occurrence.sql"), here::here("tests/unittest_etl_condition_occurrence_table.R"), TRUE, FALSE,
@@ -14,4 +14,8 @@ run_config <- tibble::tribble(
   # "measurement_table" , here::here("sql/etl_measurement.sql"), here::here("tests/unittest_etl_measurement_table.R"), TRUE, FALSE,
   # "device_exposure_table" , here::here("sql/etl_device_exposure.sql"), here::here("tests/unittest_etl_device_exposure_table.R"), TRUE, FALSE,
   # "observation_table" , here::here("sql/etl_observation.sql"), here::here("tests/unittest_etl_observation_table.R"), TRUE, FALSE,
+  # "drug_era" , here::here("sql/etl_drug_era.sql"), "", TRUE, FALSE, # set test always to FALSE
+  # "condition_era" , here::here("sql/etl_condition_era.sql"), "", TRUE, FALSE, # set test always to FALSE
+  # "cdm_source" , here::here("sql/etl_cdm_source.sql"), "", TRUE, FALSE, # set test always to FALSE
+  # "webapi" , here::here("sql/etl_webapi_bigquery.sql"), "", TRUE, FALSE, # set test always to FALSE
 )
---
+++ 3_etl_code/ETL_Orchestration/sql/etl_drug_exposure.sql
@@ -0,0 +1,170 @@
+# DESCRIPTION:
+# Creates a row in cdm-drug exposure table for each FinnGen id in the PURCH registry.
+# Person id is extracted from person table
+#
+# PARAMETERS:
+#
+# - schema_etl_input: schema with the etl input tables
+# - schema_cdm_output: schema with the output CDM tables
+
+
+TRUNCATE TABLE @schema_cdm_output.drug_exposure;
+INSERT INTO @schema_cdm_output.drug_exposure
+(
+  drug_exposure_id,
+  person_id,
+  drug_concept_id,
+  drug_exposure_start_date,
+  drug_exposure_start_datetime,
+  drug_exposure_end_date,
+  drug_exposure_end_datetime,
+  verbatim_end_date,
+  drug_type_concept_id,
+  stop_reason,
+  refills,
+  quantity,
+  days_supply,
+  sig,
+  route_concept_id,
+  lot_number,
+  provider_id,
+  visit_occurrence_id,
+  visit_detail_id,
+  drug_source_value,
+  drug_source_concept_id,
+  route_source_value,
+  dose_unit_source_value
+)
+
+WITH
+# 1 - Get all purchase events form the PURCH registry
+purchases_from_registers AS (
+  SELECT *
+  FROM(
+    SELECT FINNGENID,
+           SOURCE,
+           APPROX_EVENT_DAY,
+           CODE1_ATC_CODE AS CODE1,
+           CODE3_VNRO AS CODE3,
+           CODE4_PLKM AS CODE4,
+           INDEX
+    FROM @schema_etl_input.purch
+    #WHERE FINNGENID = 'FG00000020'
+  )
+),
+# 2 - Add vnr omop concept id
+purchases_from_registers_vnr_info AS (
+  SELECT pg.*,
+         fgc.omop_concept_id AS drug_omop_concept_id,
+         fgc.name_en AS medicine_name
+  FROM purchases_from_registers AS pg
+  LEFT JOIN @schema_table_codes_info AS fgc
+  ON fgc.FG_CODE1 IS NOT DISTINCT FROM LPAD(pg.CODE3,6,'0') AND
+     fgc.vocabulary_id = 'VNRfi'
+),
+# 3 - Add standard concept id and take the top priority
+purchases_from_registers_vnr_info_standard_concept_id AS (
+  SELECT prvi.*,
+         drugmap.concept_id_2,
+         RANK() OVER (PARTITION BY prvi.CODE3
+                            ORDER BY CASE drugmap.concept_class_id
+                                          WHEN 'Branded Pack' THEN 1
+                                          WHEN 'Clinical Pack' THEN 2
+                                          WHEN 'Branded Drug' THEN 3
+                                          WHEN 'Clinical Drug' THEN 4
+                                          WHEN 'Branded Drug Comp' THEN 5
+                                          WHEN 'Clinical Drug Comp' THEN 6
+                                          WHEN 'Branded Drug Form' THEN 7
+                                          WHEN 'Clinical Drug Form' THEN 8
+                                          WHEN 'Ingredient' THEN 9
+                                          #WHEN 'Quant Clinical Drug' THEN 9
+                                          #WHEN 'Quantified Branded Drug' THEN 10
+                                          #WHEN 'Clinical Drug Box' THEN 11
+                                          #WHEN 'Quantified Clinical Box' THEN 12
+                                          #ELSE 13
+                                          ELSE 10
+                                     END
+                           ) AS drug_priority
+  FROM purchases_from_registers_vnr_info AS prvi
+  LEFT JOIN (
+    SELECT cr.concept_id_1,
+           cr.concept_id_2,
+           c.concept_class_id,
+           c.concept_name
+    FROM @schema_vocab.concept_relationship AS cr
+    JOIN @schema_vocab.concept AS c
+    ON cr.concept_id_2 = c.concept_id
+    WHERE cr.relationship_id = 'Maps to' AND c.domain_id IN ('Drug')
+  ) AS drugmap
+  ON CAST(prvi.drug_omop_concept_id AS INT64) = drugmap.concept_id_1
+)
+
+# 4 - Shape into drug exposure table
+SELECT
+# drug_exposure_id
+ROW_NUMBER() OVER(ORDER by prvisci.SOURCE, prvisci.INDEX) AS drug_exposure_id,
+# person_id
+p.person_id AS person_id,
+# drug_concept_id
+CASE
+    WHEN prvisci.concept_id_2 IS NOT NULL THEN prvisci.concept_id_2
+		ELSE 0
+END AS drug_concept_id,
+# drug_exposure_start_date
+prvisci.APPROX_EVENT_DAY AS drug_exposure_start_date,
+# drug_exposure_start_datetime
+DATETIME(TIMESTAMP(prvisci.APPROX_EVENT_DAY)) AS drug_exposure_start_datetime,
+# drug_exposure_end_date
+prvisci.APPROX_EVENT_DAY AS drug_exposure_end_date,
+# drug_exposure_end_datetime
+DATETIME(TIMESTAMP(prvisci.APPROX_EVENT_DAY)) AS drug_exposure_end_datetime,
+# verbatim_end_date
+CAST(NULL AS DATE) AS verbatim_end_date,
+# drug_type_concept_id
+32879 AS drug_type_concept_id,
+# stop_reason
+CAST(NULL AS STRING) AS stop_reason,
+# refills
+NULL AS refills,
+# quantity
+CASE
+    WHEN prvisci.CODE4 IS NOT NULL THEN CAST(prvisci.CODE4 AS FLOAT64)
+    ELSE 0
+END AS quantity,
+# days_supply
+1 AS days_supply,
+# sig
+prvisci.medicine_name AS sig,
+# route_concept_id
+NULL AS route_concept_id,
+# lot_number
+CAST(NULL AS STRING) AS lot_number,
+# provider_id
+vo.provider_id AS provider_id,
+# visit_occurrence_id
+vo.visit_occurrence_id AS visit_occurrence_id,
+# visit_detail_id
+0 AS visit_detail_id,
+# drug_source_value
+CASE
+    WHEN CAST(prvisci.CODE3 AS INT64) > 0 THEN LPAD(prvisci.CODE3,6,'0')
+    ELSE prvisci.CODE3
+END AS drug_source_value,
+# drug_source_concept_id
+CASE
+    WHEN prvisci.drug_omop_concept_id IS NOT NULL THEN CAST(prvisci.drug_omop_concept_id AS INT64)
+    ELSE 0
+END AS drug_source_concept_id,
+# route_source_value
+CAST(NULL AS STRING) AS route_source_value,
+# dose_unit_source_value
+CAST(NULL AS STRING) AS dose_unit_source_value
+FROM purchases_from_registers_vnr_info_standard_concept_id AS prvisci
+JOIN @schema_cdm_output.person AS p
+ON p.person_source_value = prvisci.FINNGENID
+JOIN @schema_cdm_output.visit_occurrence AS vo
+ON vo.person_id = p.person_id AND
+   CONCAT('SOURCE=',prvisci.SOURCE,';INDEX=',prvisci.INDEX) = vo.visit_source_value AND
+   prvisci.APPROX_EVENT_DAY = vo.visit_start_date
+WHERE prvisci.drug_priority = 1
+ORDER BY p.person_id, prvisci.APPROX_EVENT_DAY;
