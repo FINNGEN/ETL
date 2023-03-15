7e86d1b293dea8d2376d84333bcb556ce00970e0
shanmukhasampath <shanmukhasampath@gmail.com>
2023-03-15 14:31:30 +0200

    Fixed exporting large tables to csv files.

--- 3_etl_code/ETL_Orchestration/R/bq_tables_export.sh
+++ 3_etl_code/ETL_Orchestration/R/bq_tables_export.sh
@@ -4,7 +4,7 @@
 The script takes project, dataset and storage bucket as argeuments
 and outputs all tables in the dataset into folder named output_folders
 An example script looks as below
-bash bq_tables_export.sh -p atlas-development-270609 -d etl_sam_unittest_omop -b gs://cdm_vocabulary
+bash bq_tables_export.sh -p atlas-development-270609 -d etl_sam_unittest_omop -b gs://cdm_vocabulary -v finngen_finomop_vocabulary.zip
 '
 
 
@@ -13,9 +13,10 @@ project=""
 dataset=""
 bucket=""
 outfolder=""
+vocabzip=""
 
 # Parse command line options
-while getopts ":p:d:b:" opt; do
+while getopts ":p:d:b:v:" opt; do
   case $opt in
     p)
       project=$OPTARG
@@ -25,6 +26,9 @@ while getopts ":p:d:b:" opt; do
       ;;
     b)
       bucket=$OPTARG
+      ;;
+    v)
+      vocabzip=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -41,6 +45,8 @@ done
 echo "Project: $project"
 echo "Dataset: $dataset"
 echo "Storage Bucket: $bucket"
+echo "vocabzip: $vocabzip"
+
 
 # Get output_folder name
 if [[ "$dataset" == *"_omop"* ]]; then
@@ -55,15 +61,30 @@ echo "Output Folder:  $outfolder"
 # Create the local outfolder
 [[ -d $outfolder ]] || mkdir -p ./output_folders/"$outfolder"
 
+# Get vocab zip folder name
+vocabfolder=$(echo "${vocabzip%.*}")
+echo "vocabfolder: $vocabfolder"
+
 # Get table names and schema in json format
 tables=$(bq ls --max_results 1000 "$project:$dataset" | awk 'NR>2{print $1}')
 for table in $tables
 do
-        bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/"$outfolder"/"$table".csv
-        bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/$outfolder/"$table"_schema.json
+  if [[ "$table" = "concept_ancestor" ]] && [[ "$dataset" == *"_vocab"* ]]; then
+    unzip -p $vocabzip $vocabfolder/CONCEPT_ANCESTOR.csv > ./concept_ancestor.csv
+    gsutil cp ./concept_ancestor.csv $bucket/$outfolder/concept_ancestor.csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/$outfolder/"$table"_schema.json
+    rm ./concept_ancestor.csv
+  elif [[ "$table" = "concept_relationship" ]] && [[ "$dataset" == *"_vocab"* ]]; then
+    unzip -p $vocabzip $vocabfolder/CONCEPT_RELATIONSHIP.csv > ./concept_relationship.csv
+    gsutil cp ./concept_relationship.csv $bucket/$outfolder/concept_relationship.csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/$outfolder/"$table"_schema.json
+    rm ./concept_relationship.csv
+  else
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/"$outfolder"/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/$outfolder/"$table"_schema.json
+  fi
 done
 # Download csv files from google cloud bucket to local folder
 gsutil -m cp -r $bucket/$outfolder ./output_folders
 # Upload table schema files to google bucket
 gsutil -m cp ./output_folders/$outfolder/*.json $bucket/$outfolder
-
--- 3_etl_code/ETL_Orchestration/Scripts/export_tables.sh
+++ 3_etl_code/ETL_Orchestration/Scripts/export_tables.sh
@@ -1,8 +1,8 @@
 # CDM vocabulary
-./R/bq_tables_export.sh -p atlas-development-270609 -d cdm_vocabulary -b gs://cdm_vocabulary
+./R/bq_tables_export.sh -p atlas-development-270609 -d cdm_vocabulary -b gs://cdm_vocabulary -v C:/Users/Localadmin_padmanab/Documents/FinnGen/ETLv2/finngen_finomop_vocabulary.zip
 
 # CDM output
-./R/bq_tables_export.sh -p atlas-development-270609 -d etl_sam_dev_omop -b gs://cdm_vocabulary
+./R/bq_tables_export.sh -p atlas-development-270609 -d etl_sam_dev_omop -b gs://cdm_vocabulary -v C:/Users/Localadmin_padmanab/Documents/FinnGen/ETLv2/finngen_finomop_vocabulary.zip
 
 # Achilles
-./R/bq_tables_export.sh -p atlas-development-270609 -d etl_sam_dev_achilles -b gs://cdm_vocabulary
+./R/bq_tables_export.sh -p atlas-development-270609 -d etl_sam_dev_achilles -b gs://cdm_vocabulary -v C:/Users/Localadmin_padmanab/Documents/FinnGen/ETLv2/finngen_finomop_vocabulary.zip
