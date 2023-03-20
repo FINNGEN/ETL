93a10fae3c517d96e9a3bbfa2bed8f420f1956c9
shanmukhasampath <shanmukhasampath@gmail.com>
2023-03-20 14:44:22 +0200

    Fixed export tables to csv files irrespective of size

--- 3_etl_code/ETL_Orchestration/R/bq_tables_export.sh
+++ 3_etl_code/ETL_Orchestration/R/bq_tables_export.sh
@@ -4,7 +4,7 @@
 The script takes project, dataset and storage bucket as argeuments
 and outputs all tables in the dataset into folder named output_folders
 An example script looks as below
-bash bq_tables_export.sh -p atlas-development-270609 -d etl_sam_unittest_omop -b gs://cdm_vocabulary -v finngen_finomop_vocabulary.zip
+bash bq_tables_export.sh -p atlas-development-270609 -d etl_sam_unittest_omop -b gs://cdm_vocabulary
 '
 
 
@@ -13,10 +13,9 @@ project=""
 dataset=""
 bucket=""
 outfolder=""
-vocabzip=""
 
 # Parse command line options
-while getopts ":p:d:b:v:" opt; do
+while getopts ":p:d:b:" opt; do
   case $opt in
     p)
       project=$OPTARG
@@ -26,9 +25,6 @@ while getopts ":p:d:b:v:" opt; do
       ;;
     b)
       bucket=$OPTARG
-      ;;
-    v)
-      vocabzip=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -45,46 +41,61 @@ done
 echo "Project: $project"
 echo "Dataset: $dataset"
 echo "Storage Bucket: $bucket"
-echo "vocabzip: $vocabzip"
-
-
-# Get output_folder name
-if [[ "$dataset" == *"_omop"* ]]; then
-        outfolder="cdm"
-elif [[ "$dataset" == *"_vocab"* ]]; then
-        outfolder="vocab"
-elif [[ "$dataset" == *"achilles"* ]]; then
-        outfolder="achilles"
-fi
-echo "Output Folder:  $outfolder"
 
 # Create the local outfolder
-[[ -d $outfolder ]] || mkdir -p ./output_folders/"$outfolder"
-
-# Get vocab zip folder name
-vocabfolder=$(echo "${vocabzip%.*}")
-echo "vocabfolder: $vocabfolder"
+[[ -d ./output_folders/vocab ]] || mkdir -p ./output_folders/vocab
+[[ -d ./output_folders/cdm ]] || mkdir -p ./output_folders/cdm
+[[ -d ./output_folders/achilles ]] || mkdir -p ./output_folders/achilles
 
 # Get table names and schema in json format
 tables=$(bq ls --max_results 1000 "$project:$dataset" | awk 'NR>2{print $1}')
 for table in $tables
 do
-  if [[ "$table" = "concept_ancestor" ]] && [[ "$dataset" == *"_vocab"* ]]; then
-    unzip -p $vocabzip $vocabfolder/CONCEPT_ANCESTOR.csv > ./concept_ancestor.csv
-    gsutil cp ./concept_ancestor.csv $bucket/$outfolder/concept_ancestor.csv
-    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/$outfolder/"$table"_schema.json
-    rm ./concept_ancestor.csv
-  elif [[ "$table" = "concept_relationship" ]] && [[ "$dataset" == *"_vocab"* ]]; then
-    unzip -p $vocabzip $vocabfolder/CONCEPT_RELATIONSHIP.csv > ./concept_relationship.csv
-    gsutil cp ./concept_relationship.csv $bucket/$outfolder/concept_relationship.csv
-    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/$outfolder/"$table"_schema.json
-    rm ./concept_relationship.csv
-  else
-    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/"$outfolder"/"$table".csv
-    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/$outfolder/"$table"_schema.json
+  # VOCAB
+  if [[ "$table" =~ ^(cohort|cohort_definition|concept|concept_class|concept_synonym|domain|drug_strength|relationship|source_to_concept_map|vocabulary)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/vocab/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/vocab/"$table"_schema.json
+  elif [[ "$table" =~ ^(concept_ancestor|concept_relationship)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/vocab/"$table"-*.csv
+    ./R/bq_large_tables_export.sh -b $bucket/vocab -t $table
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/vocab/"$table"_schema.json
+  # CDM
+  elif [[ "$table" =~ ^(care_site|cdm_source|cost|death|device_exposure|dose_era|episode|episode_event|fact_relationship|location|metadata|note|note_nlp|observation_period|payer_plan_period|person|provider|specimen|visit_detail)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/cdm/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/cdm/"$table"_schema.json
+  elif [[ "$table" =~ ^(condition_era|condition_occurrence|drug_era|drug_exposure|measurement|observation|procedure_occurrence|visit_occurrence)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/cdm/"$table"-*.csv
+    ./R/bq_large_tables_export.sh -b $bucket/cdm -t $table
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/cdm/"$table"_schema.json
+  # Achilles - achilles
+  elif [[ "$table" =~ ^(achilles_analysis|achilles_results|achilles_results_dist|cc_results|cohort|cohort_cache|cohort_sensor_stats|cohort_sensor_stats_cache)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/achilles/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/achilles/"$table"_schema.json
+  # Achilles - cohort
+  elif [[ "$table" =~ ^(cohort_inclusion|cohort_inclusion_result|cohort_inclusion_result_cache|cohort_inclusion_stats|cohort_inclusion_stats_cache|cohort_sample_element|cohort_summary_stats|cohort_summary_stats_cache|concept_hierarchy)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/achilles/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/achilles/"$table"_schema.json
+  # Achilles - feas
+  elif [[ "$table" =~ ^(concept_hierarchy|feas_study_inclusion_stats|feas_study_index_stats|feas_study_result)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/achilles/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/achilles/"$table"_schema.json
+  # Achilles - heracles
+  elif [[ "$table" =~ ^(heracles_analysis|heracles_heel_results|heracles_periods|heracles_results|heracles_results_dist)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/achilles/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/achilles/"$table"_schema.json
+  # Achilles - ír
+  elif [[ "$table" =~ ^(ir_analysis_dist|ir_analysis_result|ir_analysis_strata_stats|ir_strata)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/achilles/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/achilles/"$table"_schema.json
+  # Achilles - pathway
+  elif [[ "$table" =~ ^(pathway_analysis_codes|pathway_analysis_events|pathway_analysis_paths|pathway_analysis_stats)$ ]]; then
+    bq extract --destination_format "CSV" --field_delimiter "\t" --print_header=true "$project:$dataset"."$table" "$bucket"/achilles/"$table".csv
+    bq show --format=prettyjson "$project:$dataset"."$table" | jq '.schema.fields' > ./output_folders/achilles/"$table"_schema.json
   fi
 done
 # Download csv files from google cloud bucket to local folder
-gsutil -m cp -r $bucket/$outfolder ./output_folders
+#gsutil -m cp -r $bucket/$outfolder ./output_folders
 # Upload table schema files to google bucket
-gsutil -m cp ./output_folders/$outfolder/*.json $bucket/$outfolder
+gsutil -m cp ./output_folders/vocab/*.json $bucket/vocab
+gsutil -m cp ./output_folders/cdm/*.json $bucket/cdm
+gsutil -m cp ./output_folders/achilles/*.json $bucket/achilles
